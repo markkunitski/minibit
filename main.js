5fedcb3232fcf12ecfe61abb13adf27d4062ba8e
markkunitski <markkmpya@gmail.com>
2023-10-23 22:04:20 +0200

    23.10 timer

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ class TimerCalculator {
     this.seconds = seconds;
   }
   calcTimer() {
-    return this.seconds / 60 >= 60
-      ? (this.seconds / 60 / 60).toFixed(2)
-      : (this.seconds / 60).toFixed(2);
+    const hours = Math.floor(this.seconds / 3600);
+    const minutes = Math.floor((this.seconds % 3600) / 60);
+    const seconds = this.seconds % 60;
+
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   }
 }
 class Modal {
@@ -24,96 +30,269 @@ class Modal {
   }
 }
 class Fetched {
-  async getData() {
-    try {
-      let response = await fetch("http://localhost:3000/tasks");
-      if (!response.ok) {
-        throw new Error("fetch error");
+  getData() {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:3000/tasks", true);
+      xhr.onload = function () {
+        if (this.status == 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject("Error: " + this.status);
+        }
+      };
+      xhr.onerror = function () {
+        reject("Request failed");
+      };
+      xhr.send();
+    });
+  }
+}
+class Task {
+  constructor(id, title, timer, priority, deadline, completed, task) {
+    this.id = id;
+    this.title = title;
+    this.timer = timer;
+    this.priority = priority;
+    this.deadline = deadline;
+    this.completed = completed;
+    this.task = task;
+  }
+  openModal() {
+    let openFunc = new Modal(this.title, this.task);
+    openFunc.openModal();
+  }
+  removeTask() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("DELETE", `http://localhost:3000/tasks/${this.id}`, true);
+    xhr.onload = function () {
+      if (this.status != 200) {
+        throw new Error("Error while removing task");
       }
-      let data = await response.json();
-      return data;
-    } catch (err) {
-      throw new Error(err);
+    };
+    xhr.send();
+  }
+  startTimer(htmlElem) {
+    let initial = Date.now();
+    
+    let interval = setInterval(() => {
+      this.timer++; // Increment the timer
+  
+      const timerCalculator = new TimerCalculator(this.timer); // Create a new TimerCalculator object with the updated timer
+      const properTime = timerCalculator.calcTimer(); // Calculate the proper time
+  
+      console.log(this.timer);
+      console.log(properTime);
+      htmlElem.innerText = properTime; // Update the HTML element with the proper time
+    }, 1000);
+  }
+  completeTask() {
+    this.completed = true;
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", `http://localhost:3000/tasks/${this.id}`, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+      if (this.status != 200) {
+        throw new Error("Error while completing task");
+      }
+    };
+    xhr.send(JSON.stringify(this));
+  }
+}
+let taskMap = new Map();
+class fetchAndAdd {
+  doFetch() {
+    const data = new Fetched();
+    if (data) {
+      thead.innerHTML = null;
+      tbody.innerHTML = null;
+      data
+        .getData()
+        .then((data) => {
+          data.forEach((obj, index) => {
+            const task = new Task(
+              obj.id,
+              obj.title,
+              obj.timer,
+              obj.priority,
+              obj.deadline,
+              obj.completed,
+              obj.task
+            );
+            let row = document.createElement("tr");
+            const keyOrder = [
+              "id",
+              "title",
+              "timer",
+              "priority",
+              "deadline",
+              "completed",
+              "task",
+            ];
+            for (const key of keyOrder) {
+              if (key === "timer" || key === "task") continue;
+              // th
+              let node = null;
+              if (data.length - 1 === index) {
+                node = document.createElement("th");
+                node.textContent = key === "id" ? "" : key;
+              }
+              // td
+              let contentNode = document.createElement("td");
+              switch (key) {
+                case "id":
+                  contentNode.innerHTML =
+                    key === "id"
+                      ? `<label>
+                          <input type="checkbox" class="checkbox" />
+                        </label>`
+                      : obj[key];
+                  break;
+                case "title":
+                  let timer = new TimerCalculator(obj.timer);
+                  contentNode.innerHTML =
+                    key === "title"
+                      ? `<div class='flex items-center'>
+                    <button class="btn btn-neutral btn-xs me-2 timer_btn">
+                  <span class="material-icons text-sm">
+                  play_arrow
+                  </span>
+                  
+                  ${timer.calcTimer()}
+                  
+                  </button>
+                  ${obj[key]}
+                  </div>
+    
+                  `
+                      : obj[key];
+                  break;
+                case "priority":
+                  contentNode.innerHTML =
+                    key === "priority"
+                      ? `
+                    <span class="material-icons priority_icon ${obj[key]}">
+                    ${obj[key]}
+                    </span>
+                  `
+                      : obj[key];
+                  break;
+                case key:
+                  contentNode.textContent = obj[key];
+                  break;
+              }
+              if (key === "completed" && obj[key] === true) {
+                row.firstChild.classList.add("completed");
+              }
+              row.appendChild(contentNode);
+              if (node) thead.append(node);
+            }
+            let button = document.createElement("button");
+            button.textContent = "details";
+            button.className = "btn btn-neutral btn-xs";
+            button.addEventListener("click", task.openModal.bind(task));
+            // row append
+            let td = document.createElement("td");
+            td.appendChild(button);
+            row.appendChild(td);
+            row.classList.add("hover");
+            tbody.append(row);
+            taskMap.set(row, task);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }
 }
-const data = new Fetched();
-data
-  .getData()
-  .then((data) => {
-    data.forEach((obj, index) => {
-      let keys = Object.keys(obj);
-      let row = document.createElement("tr");
-      keys.forEach((key) => {
-        if (key === "timer" || key === "task") return;
-        // th
-        let node = null;
-        if (data.length - 1 === index) {
-          node = document.createElement("th");
-          node.textContent = key === "id" ? "" : key;
+class GetTask {
+  constructor(elementClass) {
+    this.elementClass = elementClass;
+  }
+  get() {
+    let rows = document.querySelectorAll("tbody tr");
+    let result = [];
+    rows.forEach((row) => {
+      let elem = row.querySelector(`.${this.elementClass}`);
+      if (!elem) return;
+      if (elem.checked) {
+        let task = taskMap.get(row);
+        if (task) {
+          result.push(task);
+          return;
         }
-        // td
-        let contentNode = document.createElement("td");
-        switch (key) {
-          case "id":
-            contentNode.innerHTML =
-              key === "id"
-                ? `<label>
-              <input type="checkbox" class="checkbox" />
-            </label>`
-                : obj[key];
-            break;
-          case "title":
-            let timer = new TimerCalculator(obj.timer);
-            contentNode.innerHTML =
-              key === "title"
-                ? `<div class='flex items-center'>
-                <button class="btn btn-neutral btn-xs me-2">
-              <span class="material-icons text-sm">
-              play_arrow
-              </span>
-              
-              ${timer.calcTimer()}
-              
-              </button>
-              ${obj[key]}
-              </div>
-
-              `
-                : obj[key];
-            break;
-          case "type":
-            contentNode.innerHTML =
-              key === "type"
-                ? `
-                <span class="material-icons ${obj[key]}">
-                ${obj[key]}
-                </span>
-              `
-                : obj[key];
-            break;
-          case key:
-            contentNode.textContent = obj[key];
-            break;
-        }
-        if (obj[key] === true){
-          row.firstChild.classList.add('completed')
-        } 
-        row.appendChild(contentNode);
-        if (node) thead.append(node);
-      });
-      let openFunc = new Modal(obj.title, obj.task);
-      let button = document.createElement("button");
-      button.textContent = "details";
-      button.className = "btn btn-neutral btn-xs";
-      button.addEventListener("click", openFunc.openModal.bind(openFunc));
-      let td = document.createElement("td");
-      td.appendChild(button);
-      row.appendChild(td);
-      row.classList.add("hover");
-      tbody.append(row);
+      }
     });
-  })
-  .catch((error) => {
-    console.log(error);
+    return result.length ? result : undefined;
+  }
+}
+complete_button.addEventListener("click", () => {
+  // get checkboxes
+  const getTask = new GetTask("checkbox");
+  getTask.get().forEach((elem) => elem.completeTask());
+  doFetch.doFetch();
+});
+
+delete_button.addEventListener("click", () => {
+  // get checkboxes
+  const getTask = new GetTask("checkbox");
+  getTask.get().forEach((elem) => elem.removeTask());
+  doFetch.doFetch();
+});
+const doFetch = new fetchAndAdd();
+doFetch.doFetch();
+// form
+form_add.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const myform = new FormData(this);
+  const taskData = { timer: "0", completed: "false" };
+  myform.forEach((elem, index) => {
+    taskData[index] = elem;
   });
+  if (Object.keys(taskData).length) {
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+    });
+  }
+});
+// btns show
+function listenerCheckboxes(event) {
+  if (event.target.classList.contains("checkbox")) {
+    let checkboxes = document.querySelectorAll(".checkbox");
+
+    function isAtLeastOneCheckboxChecked() {
+      return Array.from(checkboxes).some((checkbox) => checkbox.checked);
+    }
+    // Add event listeners to the checkboxes
+    if (checkboxes) {
+      Array.from(checkboxes).forEach((checkbox) => {
+        checkbox.addEventListener("change", () => {
+          if (isAtLeastOneCheckboxChecked()) {
+            delete_button.classList.remove("hidden");
+            complete_button.classList.remove("hidden");
+          } else {
+            delete_button.classList.add("hidden");
+            complete_button.classList.add("hidden");
+          }
+        });
+      });
+    }
+  }
+  this.removeEventListener("input", listenerCheckboxes);
+}
+function listenerTimers(event) {
+  if (event.target.classList.contains("timer_btn")) {
+    const row = event.target.closest("tr");
+    if (taskMap.has(row)) {
+      const task = taskMap.get(row);
+      task.startTimer(event.target);
+    }
+  }
+}
+main_table.addEventListener("input", listenerCheckboxes);
+main_table.addEventListener("click", listenerTimers);
